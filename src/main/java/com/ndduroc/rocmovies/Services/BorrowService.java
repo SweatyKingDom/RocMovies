c5b0e5895ee594e5d0ed72eaf659164e7f8ba7a4
SweatyKingDom <boss.sweatyyy@gmail.com>
2025-02-14 16:52:16 +0100

    last_commit_cours_2

--- src/main/java/com/ndduroc/rocmovies/Services/BorrowService.java
+++ src/main/java/com/ndduroc/rocmovies/Services/BorrowService.java
@@ -1,7 +1,6 @@
 package com.ndduroc.rocmovies.Services;
 
 import java.util.List; 
-import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.stereotype.Service; 
 import com.ndduroc.rocmovies.Entity.Borrow; 
 import com.ndduroc.rocmovies.Repositories.BorrowRepository;
@@ -9,7 +8,6 @@ import com.ndduroc.rocmovies.Repositories.BorrowRepository;
 @Service public class BorrowService implements IBorrowService {
     private final BorrowRepository borrowRepository;
 
-    @Autowired
     public BorrowService(BorrowRepository borrowRepository) {
         this.borrowRepository = borrowRepository;
         System.out.println("Création du service BorrowService");
